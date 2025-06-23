196b0ad5a72db0ae9664b5ce6450da625198083f
Your Name <your.email@example.com>
2025-06-23 19:37:23 +0100

    Fix pg-backup Dockerfile: remove unsupported -d option from crond, use -f only

--- pg-backup/backup.sh
+++ pg-backup/backup.sh
@@ -11,7 +11,7 @@ pg_dump -h auth-db -U auth_admin auth_logs_db | gzip > $BACKUP_DIR/auth_logs_db_
 # USER DB
 pg_dump -h user-db -U user_admin users_db | gzip > $BACKUP_DIR/users_db_$TIMESTAMP.sql.gz
 
-# SOCIAL DB
+# SOCIAL DB.
 pg_dump -h social-log-db -U social_admin social_logs_db | gzip > $BACKUP_DIR/social_logs_db_$TIMESTAMP.sql.gz
 
 # Conserva solo gli ultimi 5 backup per ogni DB, cancella i più vecchi
